a43b9ce988b2bdd54fadb2112f52f4b06cbad5ba
dearblue <dearblue@users.noreply.github.com>
2019-01-14 14:52:50 +0900

    YAML による共通化

--- test_config.rb
+++ test_config.rb
@@ -1,58 +1,52 @@
 #!ruby
 
-MRuby::Build.new do |conf|
-  toolchain :clang
+require "yaml"
 
-  conf.build_dir = "host32"
+config = YAML.load <<'YAML'
+  common:
+    gems:
+    - :core: "mruby-sprintf"
+    - :core: "mruby-print"
+    - :core: "mruby-bin-mrbc"
+    - :core: "mruby-bin-mirb"
+    - :core: "mruby-bin-mruby"
+  builds:
+    host:
+      defines: MRB_INT64
+      gems:
+      - :core: "mruby-io"
+    host-int32:
+      defines: MRB_INT32
+    host-nan-int16:
+      defines: [MRB_INT16, MRB_NAN_BOXING]
+    host-word:
+      defines: MRB_WORD_BOXING
+    #  c++error: true
+YAML
 
-  if cc.command =~ /\b(?:g?cc|clang)\d*\b/
-    cc.flags << "-std=c11"
-    cc.flags << "-pedantic"
-    cc.flags << "-Wall"
+config["builds"].each_pair do |n, c|
+  MRuby::Build.new(n) do |conf|
+    toolchain :clang
+
+    conf.build_dir = c["build_dir"] || name
+
+    enable_debug
+    enable_test
+    enable_cxx_abi if c["c++abi"]
+    enable_cxx_exception if c["c++error"]
+
+    cc.defines << [*c["defines"]]
+    cc.flags << [*c["cflags"]]
+
+    Array(config.dig("common", "gems")).each { |*g| gem *g }
+    Array(c["gems"]).each { |*g| gem *g }
+
+    gem File.dirname(__FILE__) do |g|
+      if g.cc.command =~ /\b(?:g?ccc|clang)\d*\b/
+        g.cc.flags << "-std=c11" unless c["c++abi"]
+        g.cc.flags << "-pedantic"
+        g.cc.flags << "-Wall"
+      end
+    end
   end
-
-  cc.defines << "MRB_INT32"
-
-  enable_debug
-  enable_test
-
-  gem core: "mruby-print"
-  gem core: "mruby-bin-mrbc"
-  gem core: "mruby-bin-mirb"
-  gem core: "mruby-bin-mruby"
-  gem File.dirname(__FILE__)
-end
-
-MRuby::Build.new("host-nan16") do |conf|
-  toolchain :clang
-
-  conf.build_dir = conf.name
-
-  enable_debug
-  enable_test
-
-  cc.defines << "MRB_NAN_BOXING"
-  cc.defines << "MRB_INT16"
-
-  gem core: "mruby-print"
-  gem core: "mruby-bin-mrbc"
-  gem core: "mruby-bin-mruby"
-  gem File.dirname(__FILE__)
-end
-
-MRuby::Build.new("host-word") do |conf|
-  toolchain :clang
-
-  conf.build_dir = conf.name
-
-  cc.defines << "MRB_WORD_BOXING"
-  cc.defines << "MRB_INT64" if [nil].pack("P").bytesize == 8
-
-  enable_debug
-  enable_test
-
-  gem core: "mruby-print"
-  gem core: "mruby-bin-mrbc"
-  gem core: "mruby-bin-mruby"
-  gem File.dirname(__FILE__)
 end
